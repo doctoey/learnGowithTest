acdecca586c7b634adea0ac36f640b526ec633c2
doctoey <doctoey@odds.team>
2026-08-02 10:07:59 +0700

    refactor: simplify greeting logic in hello.go

--- hello.go
+++ hello.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 // Hello รับค่า string มา 1 ค่า แล้ว return ค่า string ที่ต่อท้ายด้วย "Hello, " + name
 func Hello(name string) string {
 	// return ค่า string ที่ต่อท้ายด้วย "Hello, " + name
-	return "Hello, " + name
+	return englishHelloPrefix + name
 }
 
 func main() {
