ebb6150650874fea666820cdc0123465c253f27c
doctoey <doctoey@odds.team>
2026-08-02 14:20:15 +0700

    refactor: update wallet deposit and balance types to use Bitcoin type alias

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -2,8 +2,10 @@ package wallet
 
 import "fmt"
 
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // ก่อนใช้ pointer
@@ -18,11 +20,11 @@ type Wallet struct {
 // หลักการของ pointer
 // 1. * คือการ dereference คือการเข้าถึงค่าของ pointer
 // 2. & คือการ reference คือการเข้าถึง address ของตัวแปร
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
