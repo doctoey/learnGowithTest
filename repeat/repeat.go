a06970acd34c4c3e1153cd0bd8077c11d38ee1a5
doctoey <doctoey@odds.team>
2026-08-02 11:28:13 +0700

    test: add benchmark for Repeat function

--- repeat/repeat.go
+++ repeat/repeat.go
@@ -1,11 +1,14 @@
 package repeat
 
+import "strings"
+
 const repeatCount = 5
 
 func Repeat(char string) string {
-	var repeated string
+	var repeated strings.Builder
+	// repeated.Grow(len(char) * repeatCount) // จองหน่วยความจำล่วงหน้าทีเดียว
 	for i := 0; i < repeatCount; i++ {
-		repeated += char
+		repeated.WriteString(char)
 	}
-	return repeated
+	return repeated.String()
 }
