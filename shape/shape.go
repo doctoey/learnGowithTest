330bfb8c00fe40e0bde717f59bd3b6ec575c9729
doctoey <doctoey@odds.team>
2026-08-02 13:47:53 +0700

    refactor: introduce Shape interface and implement Area calculation for Rectangle and Circle types

--- shape/shape.go
+++ shape/shape.go
@@ -1,9 +1,41 @@
 package shape
 
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
 }
 
-func Area(width, height float64) float64 {
-	return (width * height)
+type Circle struct {
+	Radius float64
+}
+
+// interface ชี้ไปที่ method ใด ๆ ก็ได้ที่มี Area() float64
+// เหมือน abstract class ใน java
+// Golang ไม่มี abstract class
+// Golang ไม่มี inheritance
+// Golang ใช้ interface แทน
+type Shape interface {
+	Area() float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Height + rectangle.Width)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return (rectangle.Height * rectangle.Width)
+}
+
+func (r Rectangle) Area() float64 {
+	return (r.Height * r.Width)
+}
+
+// method ของ circle
+// () คือ receiver คือตัวแปร c ชี้ไปที่ Circle
+// Area() คือ method
+// ถ้าเป็นชื่อเดียวกับ function ตัวบนจะเรียก method ไม่ได้
+func (c Circle) Area() float64 {
+	return (c.Radius * c.Radius) * math.Pi
 }
