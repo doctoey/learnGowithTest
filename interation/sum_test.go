601336bc523c94cb2ea02309b8b87a26d696f4f7
doctoey <doctoey@odds.team>
2026-08-02 11:44:35 +0700

    refactor: update Sum function to accept slices instead of fixed-size arrays and expand test cases

--- interation/sum_test.go
+++ interation/sum_test.go
@@ -3,12 +3,26 @@ package interation
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+	t.Run("5 number ", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("any number ", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 }
