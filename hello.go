a6befb1160eb12912ac08b4a9c136592ef17784a
doctoey <doctoey@odds.team>
2026-08-02 10:14:17 +0700

    feat: implement personalized greeting functionality with corresponding unit tests

--- hello.go
+++ hello.go
@@ -7,6 +7,10 @@ const englishHelloPrefix = "Hello, "
 // Hello รับค่า string มา 1 ค่า แล้ว return ค่า string ที่ต่อท้ายด้วย "Hello, " + name
 func Hello(name string) string {
 	// return ค่า string ที่ต่อท้ายด้วย "Hello, " + name
+	// เพิ่มเงื่อนไขว่าถ้า name เป็น "" ให้ name = "World"
+	if name == "" {
+		name = "World"
+	}
 	return englishHelloPrefix + name
 }
 
--- hello_test.go
+++ hello_test.go
@@ -5,16 +5,27 @@ import "testing"
 func TestHello(t *testing.T) {
 	// เพิ่มตัวแปร name เข้าไปในฟังก์ชัน Hello
 	// name เก็บค่า "Toey" เพื่อส่งเข้าไปในฟังก์ชัน Hello
-	got := Hello("Toey")
-	want := "Hello, Toey"
+	t.Run("saying Hello to Toey", func(t *testing.T) {
+		got := Hello("Toey")
+		want := "Hello, Toey"
 
-	if got != want {
-		// t ย่อมาจาก testing
-		// Errorf  ย่อมาจาก error format
-		// got เก็บค่าที่ได้จากการ return
-		// want เก็บค่าที่คาดหวัง
-		// %q ใช้ในการแสดงผล string แบบมีเครื่องหมายคำพูดครอบ เช่น "Hello, world"
-		// t.Errorf คือ การแจ้งเตือนว่า test fail พร้อมบอกรายละเอียด
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			// t ย่อมาจาก testing
+			// Errorf  ย่อมาจาก error format
+			// got เก็บค่าที่ได้จากการ return
+			// want เก็บค่าที่คาดหวัง
+			// %q ใช้ในการแสดงผล string แบบมีเครื่องหมายคำพูดครอบ เช่น "Hello, world"
+			// t.Errorf คือ การแจ้งเตือนว่า test fail พร้อมบอกรายละเอียด
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
