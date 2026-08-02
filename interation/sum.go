fa77bd9dde53b72a728a9f5ff4a1c8384013b097
doctoey <doctoey@odds.team>
2026-08-02 12:04:00 +0700

    refactor: simplify sum loop using the range keyword

--- interation/sum.go
+++ interation/sum.go
@@ -10,21 +10,29 @@ func Sum(numbers []int) int {
 
 func SumAll(numbersTosum ...[]int) []int {
 	// lengthOfNumbers เก็บจำนวนของตัวแปร numbersTosum ที่ส่งเข้ามา
-	lengthOfNumbers := len(numbersTosum)
-	// make คือ การจองพื้นที่ในหน่วยความจำ ให้กับตัวแปร sums
-	// โดยกำหนดให้มีขนาดเท่ากับจำนวนของตัวแปร numbersTosum
-	// ไม่ต้องใส่ค่า default เพราะเราจะใช้ค่าเริ่มต้นคือ 0 อยู่แล้ว
-	// capacity คือ จำนวนครั้งที่เราสามารถเพิ่มค่าได้โดยไม่ต้องจองพื้นที่ใหม่
-	// ในที่นี้เราจองพื้นที่ไว้ 2 เท่าของจำนวนของตัวแปร numbersTosum (lengthOfNumbers*2)
-	// cap กับ length ต่างกันยังไง
-	// length คือ ขนาดของตัวแปร
-	// capacity คือ ขนาดของ array ที่จองไว้
-	// ถ้าเกินน่าจะ panic
+	// lengthOfNumbers := len(numbersTosum)
+	// // make คือ การจองพื้นที่ในหน่วยความจำ ให้กับตัวแปร sums
+	// // โดยกำหนดให้มีขนาดเท่ากับจำนวนของตัวแปร numbersTosum
+	// // ไม่ต้องใส่ค่า default เพราะเราจะใช้ค่าเริ่มต้นคือ 0 อยู่แล้ว
+	// // capacity คือ จำนวนครั้งที่เราสามารถเพิ่มค่าได้โดยไม่ต้องจองพื้นที่ใหม่
+	// // ในที่นี้เราจองพื้นที่ไว้ 2 เท่าของจำนวนของตัวแปร numbersTosum (lengthOfNumbers*2)
+	// // cap กับ length ต่างกันยังไง
+	// // length คือ ขนาดของตัวแปร
+	// // capacity คือ ขนาดของ array ที่จองไว้
+	// // ถ้าเกินน่าจะ panic
 
-	sums := make([]int, lengthOfNumbers, lengthOfNumbers*2)
+	// sums := make([]int, lengthOfNumbers, lengthOfNumbers*2)
 
-	for i, numbers := range numbersTosum {
-		sums[i] = Sum(numbers)
+	// for i, numbers := range numbersTosum {
+	// 	sums[i] = Sum(numbers)
+	// }
+	// return sums
+
+	var sums []int
+	for _, numbers := range numbersTosum {
+		sums = append(sums, Sum(numbers))
 	}
+
 	return sums
+
 }
