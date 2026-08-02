6a229478e8da8b2856dd453597cb57cb8eda924e
doctoey <doctoey@odds.team>
2026-08-02 10:20:50 +0700

    test: update hello_test to reflect recent changes in hello function logic

--- hello_test.go
+++ hello_test.go
@@ -9,23 +9,27 @@ func TestHello(t *testing.T) {
 		got := Hello("Toey")
 		want := "Hello, Toey"
 
-		if got != want {
-			// t ย่อมาจาก testing
-			// Errorf  ย่อมาจาก error format
-			// got เก็บค่าที่ได้จากการ return
-			// want เก็บค่าที่คาดหวัง
-			// %q ใช้ในการแสดงผล string แบบมีเครื่องหมายคำพูดครอบ เช่น "Hello, world"
-			// t.Errorf คือ การแจ้งเตือนว่า test fail พร้อมบอกรายละเอียด
-			t.Errorf("got %q want %q", got, want)
-		}
+		// เรียกใช้ฟังก์ชัน assertCorrectMessage เพื่อตรวจสอบค่าที่ได้จากการ return
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+// ตัวเล็ก private func
+// t testing.TB - ใช้สำหรับ test
+// @got - ค่าที่ได้จากการ return
+// @want - ค่าที่คาดหวัง
+// %q ใช้ในการแสดงผล string แบบมีเครื่องหมายคำพูดครอบ เช่น "Hello, world"
+// t.Helper() - บอกว่าฟังก์ชันนี้เป็น helper function ทำให้ test ที่ล้มเหลวจะรายงานผลไปยังฟังก์ชันที่เรียกใช้ฟังก์ชันนี้
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
